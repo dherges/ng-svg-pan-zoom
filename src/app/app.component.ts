dd582d12817d6fc7bed6aeb2cc7fa3e9c347275c
David <david@spektrakel.de>
2017-12-15 23:06:19 +0100

    feat: scale around center point :-)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public zoom = 1.8;
+  public translate = { x: -50, y: -20 };
+
+  public get svgTransform(): string {
+    const centerX = 50;
+    const centerY = 0;
+
+    const offX = -centerX * (this.zoom - 1);
+    const offY = -centerY * (this.zoom - 1);
+    const trsl = `translate(${offX}, ${offY})`;
+
+    return `${trsl} translate(${this.translate.x}, ${this.translate.y}) scale(${this.zoom}, ${this.zoom})`;
+  }
+
 }
